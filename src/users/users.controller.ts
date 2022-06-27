a7948b2173656d8c32825063f50cd18b35c7d504
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-06-27 16:58:05 -0300

    feature(users): created the getAll resource

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Post } from "@nestjs/common";
 import { UsersService } from "./users.service";
 
-@Controller('users')
+@Controller('/users')
 export class UserController {
-  constructor (private readonly usersService: UsersService) {
+  constructor (private readonly usersService: UsersService) {}
 
+  @Get()
+  getAll() {
+    return this.usersService.getAll()
+  }
+
+  @Post()
+  create() {
+    return this.usersService.create()
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,15 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { User } from './entities/users.entities';
 
 @Injectable()
 export class UsersService {
+  users: User[] = [];
 
+  getAll() {
+    return this.users;
+  }
+
+  create() {
+    return 'criação';
+  }
 }
