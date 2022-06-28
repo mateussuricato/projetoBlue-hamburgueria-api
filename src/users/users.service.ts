a5a740215942f1e586375ee4e2977dbebd60c341
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-06-27 23:26:45 -0300

    novos users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,11 +5,23 @@ import { uuid } from 'uuidv4';
 
 @Injectable()
 export class UsersService {
-  users: User[] = [{
-    name: "Mateus",
-    email: "mateus@hotmail.com",
-    id: uuid()
-  }];
+  users: User[] = [
+    {
+      name: 'Mateus',
+      email: 'mateus@hotmail.com',
+      id: uuid(),
+    },
+    {
+      name: 'Marcos',
+      email: 'marcos@hotmail.com',
+      id: uuid(),
+    },
+    {
+      name: 'Wesley',
+      email: 'Wesley@hotmail.com',
+      id: uuid(),
+    },
+  ];
 
   getAll() {
     return this.users;
