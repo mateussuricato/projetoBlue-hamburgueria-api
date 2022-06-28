3627008573bbf48dcb74c7b01e666e3b6c8ad0c1
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-06-27 21:34:45 -0300

    exercicios completos

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Modulo 4')
+    .setDescription('Aplicação do Modulo 4 Blue Backend')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3333);
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger"
+import { IsEmail, IsNotEmpty, IsString } from "class-validator"
+
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: "User name",
+    example: "Mateus",
+  })
   name: string
+
+  @IsEmail()
+  @ApiProperty({
+    description: "User email",
+    example: "mateus@hotmail.com",
+  })
   email: string
 }
