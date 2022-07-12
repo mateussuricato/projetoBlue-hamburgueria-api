e0f1d934dfa41b710b6a69fb5f5ab839eb3229be
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-07-12 14:00:44 -0300

    feature: created the id error method at the categoruesService

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { handleError } from 'src/utils/handle-error-unique.util';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
@@ -8,7 +8,7 @@ import { Category } from './entities/category.entity';
 @Injectable()
 export class CategoriesService {
   constructor(private readonly prisma: PrismaService) {}
- async create(dto: CreateCategoryDto): Promise<Category> {
+  async create(dto: CreateCategoryDto): Promise<Category> {
     return this.prisma.category.create({ data: dto }).catch(handleError);
   }
 
@@ -16,15 +16,33 @@ export class CategoriesService {
     return this.prisma.category.findMany();
   }
 
+  async verifyId(id: string): Promise<Category> {
+    const category: Category = await this.prisma.category.findUnique({
+      where: { id },
+    });
+
+    if (!category) {
+      throw new NotFoundException(`Entrada de id ${id} nao encontrada`);
+    }
+
+    return category;
+  }
+
   findOne(id: string): Promise<Category> {
-    return this.prisma.category.findUnique({ where: { id } });
+    return this.verifyId(id);
   }
 
- async update(id: string, dto: UpdateCategoryDto): Promise<Category> {
-    return this.prisma.category.update({ where: { id }, data: dto }).catch(handleError);
+  async update(id: string, dto: UpdateCategoryDto): Promise<Category> {
+    await this.verifyId(id);
+
+    return this.prisma.category
+      .update({ where: { id }, data: dto })
+      .catch(handleError);
   }
 
-  remove(id: string) {
+ async remove(id: string) {
+    await this.verifyId(id);
+
     return this.prisma.category.delete({
       where: { id },
       select: { name: true },
