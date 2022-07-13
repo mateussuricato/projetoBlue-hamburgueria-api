5d5d9e041e9491223502bc9ed7820cf34658e9a5
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-07-12 23:04:30 -0300

    feature: created the create order resource

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,10 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 
 @Injectable()
 export class OrdersService {
+  constructor(private readonly prisma: PrismaService) {}
   create(dto: CreateOrderDto) {
-    return 'This action adds a new order';
+    const data: Prisma.OrderCreateInput = {
+      table: {
+        connect: {
+          number: dto.tableNumber,
+        },
+      },
+      user: {
+        connect: {
+          id: dto.userId,
+        },
+      },
+      products: {
+        connect: dto.products.map((element) => {
+          return { id: element };
+        }),
+      },
+    };
+
+    return this.prisma.order.create({ data, select: {
+      id: true,
+      tableNumber: true,
+      createdAt: true,
+      userId: true,
+      products: {
+        select: {
+          name: true
+        }
+      }
+    } });
   }
 
   findAll() {
