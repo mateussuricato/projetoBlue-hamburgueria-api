ad69d7237f510d7a647452f50fd8dd9e430638cf
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-07-17 18:51:59 -0300

    feature: crated the unfavorite products resource

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { FavoriteProductDto } from './dto/favorite-product.dto';
+import { Favorite } from './entities/favorite.entity';
 import { FavoritesService } from './favorites.service';
 
 @ApiTags('favorites')
@@ -9,7 +10,13 @@ export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
 
   @Post()
-  favoriteProduct(@Body() dto: FavoriteProductDto) {
-    return this.favoritesService.favoriteProduct(dto)
+  favoriteProduct(@Body() dto: FavoriteProductDto): Promise<Favorite> {
+    return this.favoritesService.favoriteProduct(dto);
+  }
+
+  @Delete(":id")
+  @HttpCode(HttpStatus.NO_CONTENT)
+  unfavoriteProduct(@Param('id') id: string) {
+    return this.favoritesService.unfavoriteProduct(id)
   }
 }
--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { FavoriteProductDto } from './dto/favorite-product.dto';
+import { Favorite } from './entities/favorite.entity';
 
 @Injectable()
 export class FavoritesService {
   constructor(private readonly prisma: PrismaService) {}
-  favoriteProduct(dto: FavoriteProductDto) {
+  favoriteProduct(dto: FavoriteProductDto): Promise<Favorite> {
     const data: Prisma.FavoriteCreateInput = {
       user: {
         connect: {
@@ -21,5 +22,9 @@ export class FavoritesService {
     };
 
     return this.prisma.favorite.create({ data });
+  }
+
+  unfavoriteProduct(id: string) {
+    return this.prisma.favorite.delete({ where: { id } });
   }
 }
