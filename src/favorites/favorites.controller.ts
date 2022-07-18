914b4d9e3469d6196dcd51645b63da452684435a
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-07-18 17:51:33 -0300

    feature: created the auth strategy

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,44 +1,57 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { FavoriteProductDto } from './dto/favorite-product.dto';
 import { Favorite } from './entities/favorite.entity';
 import { FavoritesService } from './favorites.service';
 
+@UseGuards(AuthGuard())
 @ApiTags('favorites')
+@ApiBearerAuth()
 @Controller('favorites')
 export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
 
   @Post()
   @ApiOperation({
-    summary: "Favoritar produto"
+    summary: 'Favoritar produto',
   })
   favoriteProduct(@Body() dto: FavoriteProductDto): Promise<Favorite> {
     return this.favoritesService.favoriteProduct(dto);
   }
 
-  @Delete(":id")
+  @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   @ApiOperation({
-    summary: "Desfavoritar produto"
+    summary: 'Desfavoritar produto',
   })
   unfavoriteProduct(@Param('id') id: string) {
-    return this.favoritesService.unfavoriteProduct(id)
+    return this.favoritesService.unfavoriteProduct(id);
   }
 
   @Get('user/:id')
   @ApiOperation({
-    summary: "Listar produtos favoritos de um usuário"
+    summary: 'Listar produtos favoritos de um usuário',
   })
   getUserFavorites(@Param('id') id: string): Promise<Favorite[]> {
-    return this.favoritesService.getUserFavorites(id)
+    return this.favoritesService.getUserFavorites(id);
   }
 
   @Get('product/:id')
   @ApiOperation({
-    summary: "Listar todos os usuários que favoritaram um determinado produto"
+    summary: 'Listar todos os usuários que favoritaram um determinado produto',
   })
   getUsersWhoFavoritedProduct(@Param('id') id: string) {
-    return this.favoritesService.getUsersWhoFavoritedProduct(id)
+    return this.favoritesService.getUsersWhoFavoritedProduct(id);
   }
 }
